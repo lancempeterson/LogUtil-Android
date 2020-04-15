899370a5c34eb4709883d2a16dfa9f3c3e504eb0
Lance M. Peterson <lancepeterson50@gmail.com>
2020-04-15 00:03:43 -0500

    Update LogDebug.kt

--- logutil/src/main/java/com/appfrost/logutil/LogDebug.kt
+++ logutil/src/main/java/com/appfrost/logutil/LogDebug.kt
@@ -2,7 +2,7 @@ package com.appfrost.logutil
 import android.util.Log
 class LogDebug {
     private val TAG: String = "LOG_UTIL"
-    fun d(message : String){
+    fun log(message : String){
         Log.d(TAG,message)
     }
 }
